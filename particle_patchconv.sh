1d0cd0d757f5cadb17b5ea28c06624b356298558
TiloSchuetz <schuetz.tilo@gmail.com>
2026-06-17 12:59:42 +0200

    exp update

--- particle_patchconv.sh
+++ particle_patchconv.sh
@@ -2,10 +2,10 @@
 
 #SBATCH --ntasks=1
 #SBATCH --mem=400gb
-#SBATCH --job-name=scratch
+#SBATCH --job-name=patch
 #SBATCH --gres=gpu:1
-#SBATCH --partition=accelerated
-#SBATCH --time=1:00:00
+#SBATCH --partition=accelerated-h100
+#SBATCH --time=12:00:00
 
 source $(ws_find einspace_ws)/einspace_venv/bin/activate
 python einspace/main.py --config configs/particle/re_patchconv_particle.yaml --device cuda:0
--- particle_scratch.sh
+++ particle_scratch.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #SBATCH --ntasks=1
-#SBATCH --mem=700gb
+#SBATCH --mem=400gb
 #SBATCH --job-name=scratch
 #SBATCH --gres=gpu:1
-#SBATCH --partition=accelerated
-#SBATCH --time=24:00:00
+#SBATCH --partition=dev_accelerated
+#SBATCH --time=00:30:00
 
 source $(ws_find einspace_ws)/einspace_venv/bin/activate
 python einspace/main.py --config configs/particle/re_scratch_particle.yaml --device cuda:0
